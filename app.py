b013fd9529a66eb68dbc24c9ca09a2019137f5a5
Timur Siraziev <tim2022uk@outlook.com>
2024-11-26 10:18:27 +0000

    Fix app.py for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from site_checker import is_valid_domain, check_website_with_virustotal, get_whois_info
+import os
 
 app = Flask(__name__)
 
@@ -10,15 +11,19 @@ def home():
 @app.route('/check', methods=['POST'])
 def check():
     domain = request.form.get('domain').strip()
-    if is_valid_domain(domain):
-        result = f"'{domain}' is a valid domain name.<br>"
-        result += check_website_with_virustotal(domain)
-        result += "<br><strong>WHOIS Information:</strong><br>"
-        result += get_whois_info(domain)
-    else:
-        result = f"'{domain}' is not a valid domain name. Please try again."
+    try:
+        if is_valid_domain(domain):
+            result = f"'{domain}' is a valid domain name.<br>"
+            result += check_website_with_virustotal(domain)
+            result += "<br><strong>WHOIS Information:</strong><br>"
+            result += get_whois_info(domain)
+        else:
+            result = f"'{domain}' is not a valid domain name. Please try again."
+    except Exception as e:
+        result = f"An error occurred: {str(e)}"
     return result
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000, debug=True)
-
+    # Указываем порт, предоставленный Render, или используем 5000 по умолчанию
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
